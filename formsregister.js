e393098840dfdf26a0b05e2f84fb538bffc80165
VolodymyrZhyla <43322330+VolodymyrZhyla@users.noreply.github.com>
2020-02-16 22:46:48 +0200

    Add files via upload

---
+++ formsregister.js
@@ -0,0 +1,9 @@
+var button = document.getElementById('myImage')
+button.onclick = function() {
+  button.innerHTML = "Turn on the light";
+};
+
+var button = document.getElementById('myImage')
+button.onclick = function() {
+  button.innerHTML = "Turn off the light";
+};
---
+++ vitusia.js
@@ -0,0 +1,27 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
+}
+
+$('.counter-count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 10000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
